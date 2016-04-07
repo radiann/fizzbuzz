10ae6623b5782b31346982325737fdbee54aa4aa
radiann <yuzukei-koumakyou@excite.co.jp>
2016-04-07 19:04:19 +0900

    chanfe file

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,20 +1,18 @@
 <?php
-  echo "Fizz Buzz 問題<br />\n";
+  echo "世界のナベアツ 問題<br />\n";
 
   for($i = 1; $i<101; $i++){
     $quotBy3 =floor($i / 3);
     $remBy3 = $i - ($quotBy3 * 3);
-    $quotBy5 =floor($i / 5);
-    $remBy5 = $i - ($quotBy5 * 5);
+    $aho = strstr($i,'3');
 
-    if ($remBy3 == 0 && $remBy5 == 0){
-     echo "FizzBuzz<br />";}
-  else  if($remBy3 == 0 ){
-      echo "Fizz<br />";
-    } else if ($remBy5 == 0){
-        echo "Buzz<br />";
+
+   if($remBy3 == 0 ){
+      echo "aho<br />\n";
+    } else if ($aho != 0){
+        echo "aho<br />\n";
     }else {
-      echo  "$i<br />";
+      echo  "$i<br />\n";
     }
    }
       ?>
